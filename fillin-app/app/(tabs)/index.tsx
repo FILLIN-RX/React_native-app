b145d1f1c0e6de7e93e936cd47ba9042f5f17fa9
RUXEL <djeutchouruxel@gmail.com>
2025-06-27 02:20:49 +0100

    adding phoo functionaliy

--- fillin-app/app/(tabs)/index.tsx
+++ fillin-app/app/(tabs)/index.tsx
@@ -1,39 +1,45 @@
-import { Text, View,StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
-import { Image } from "expo-image";
+import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
-const placeholder = require('@/assets/images/background-image.png')
+const placeholder = require("@/assets/images/background-image.png");
 export default function Index() {
   return (
-   
     <View style={styles.container}>
-      <View style={styles.imageContainer}> 
-       <ImageViewer imgSource={placeholder}/>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={placeholder} />
       </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+
       <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>Go to about screen</Link>
+      <Link href="/about" style={styles.button}>
+        Go to about screen
+      </Link>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center'
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  text : {
-    color: '#fff'
+  text: {
+    color: "#fff",
   },
-  button:{
-      padding: 5,
-      borderRadius: 10,
-      fontSize: 20,
-      textDecorationLine: 'underline',
-      color: "red"
+  button: {
+    padding: 5,
+    borderRadius: 10,
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "red",
   },
-    imageContainer: {
+  imageContainer: {
     flex: 1,
   },
   image: {
@@ -41,5 +47,8 @@ const styles = StyleSheet.create({
     height: 440,
     borderRadius: 18,
   },
-
-})
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+});
