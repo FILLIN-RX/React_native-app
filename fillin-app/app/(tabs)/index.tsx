944bc329827f2a538b0b8480e6edd1e26cb875c8
RUXEL <djeutchouruxel@gmail.com>
2025-06-27 01:34:20 +0100

    adding phoo functionaliy

--- fillin-app/app/(tabs)/index.tsx
+++ fillin-app/app/(tabs)/index.tsx
@@ -1,8 +1,15 @@
 import { Text, View,StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+import ImageViewer from "@/components/ImageViewer";
+const placeholder = require('@/assets/images/background-image.png')
 export default function Index() {
   return (
+   
     <View style={styles.container}>
+      <View style={styles.imageContainer}> 
+       <ImageViewer imgSource={placeholder}/>
+      </View>
       <Text style={styles.text}>Home screen</Text>
       <Link href="/about" style={styles.button}>Go to about screen</Link>
     </View>
@@ -25,5 +32,14 @@ const styles = StyleSheet.create({
       fontSize: 20,
       textDecorationLine: 'underline',
       color: "red"
-  }
+  },
+    imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+
 })
